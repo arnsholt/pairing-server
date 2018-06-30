2059ce94c2a8ae474c67c31f34b8afed8c6680bd
Arne Skjærholt <arnsholt@gmail.com>
2018-06-30 20:07:16 +0200

    Small improvement to test script.

--- test-client/test-client.py
+++ test-client/test-client.py
@@ -8,7 +8,7 @@ stub = service_pb2_grpc.PairingServerStub(channel)
 # Create tournament
 t1 = Tournament()
 t1.name = "abcdefg"
-t1.rounds = 3
+t1.rounds = 1
 ident = stub.CreateTournament(t1)
 
 t2 = stub.GetTournament(ident)
@@ -21,3 +21,5 @@ else:
 # Read tournament back and make sure it's identical.
 # Sign up some players, read back player list and make sure they match up.
 # Pair round and read back games. Run some kind of basic sanity check?
+# Pair *another* round, and make sure we get an error (since we've specified
+# only a single round).
