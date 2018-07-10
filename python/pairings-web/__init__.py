ab360d94e3437ce8df533ae55462349de5778e96
Arne Skjærholt <arnsholt@gmail.com>
2018-07-10 16:10:29 +0200

    Restart work on web interface in brave new GRPC world.

--- python/pairings-web/__init__.py
+++ python/pairings-web/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask, redirect, render_template, request, url_for
-import socket
-import struct
+import grpc
 
-from requests_pb2 import Request, Response
-import types_pb2
+from .types_pb2 import Tournament, Player, Game
+from .service_pb2_grpc import PairingServerStub
 
 app = Flask(__name__)
+stub = PairingServerStub(grpc.insecure_channel("localhost:1234"))
 
 @app.route('/')
 def index():
@@ -13,45 +13,22 @@ def index():
 
 @app.route('/tournament', methods=['POST'])
 def tournament_post():
-    req = Request()
-    req.operation = Request.TOURNAMENT_CREATE
     # TODO: Data validation.
-    req.tournament.name = request.form['name']
-    req.tournament.rounds = int(request.form['rounds'])
+    tournament = Tournament()
+    tournament.name = request.form['name']
+    tournament.rounds = int(request.form['rounds'])
 
-    # TODO: Error handling?
-    resp = send_request(req)
-    return redirect(url_for('tournament', uuid=resp.tournaments[0].uuid,
-        hmac=resp.tournaments[0].hmac))
+    try:
+        response = stub.CreateTournament(tournament)
+    except:
+        # TODO: Error handling.
+        raise
+
+    return redirect(url_for('tournament', uuid=response.uuid.hex(),
+        hmac=response.hmac.digest.hex()))
 
 @app.route('/tournament/<uuid>/')
 @app.route('/tournament/<uuid>/<hmac>/')
 def tournament(uuid, hmac=None):
     write_mode = hmac is not None
     return render_template('tournament.html', write_mode=write_mode)
-
-def send_request(req):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("127.0.0.1", 1234))
-    buf = req.SerializeToString()
-    s.send(struct.pack("!L%ds" % len(buf), len(buf), buf))
-    s.shutdown(socket.SHUT_WR)
-
-    respsize = struct.unpack("!L", read_bytes(s, 4))[0]
-    resp = Response()
-    resp.ParseFromString(read_bytes(s, respsize))
-    try:
-        s.shutdown(socket.SHUT_RD)
-        s.close()
-    except OSError:
-        pass
-    return resp
-
-def read_bytes(s, count):
-    bytes_left = count
-    data = b""
-    while bytes_left > 0:
-        got = s.recv(bytes_left)
-        data = data + got
-        bytes_left -= len(got)
-    return data
